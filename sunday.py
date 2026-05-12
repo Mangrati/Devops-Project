d88de4886f0f868484ab116de1d05d9904d5c503
Muna Mangrati <139782242+Mangrati@users.noreply.github.com>
2026-05-12 22:40:03 +0545

    Add descriptions for Wednesday to Saturday

--- sunday.py
+++ sunday.py
@@ -1,3 +1,8 @@
 Sunday is the first day of the week.
 Monday is the second day of the week.
 Tuesday is the third day of the week.
+Wednesday is the fourth day of the week.
+Thrusday is the fifth day of the week.
+Friday is the sixth day of the week.
+Saturday is the seventh day of the week.
+
